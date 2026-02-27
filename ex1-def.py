e3e3c14e481cc33214439ea75251d1a3348dcce4
Cristiano Pires Martins <cpmart@gmail.com>
2026-02-27 19:19:25 -0300

    primeiro commit

---
+++ ex1-def.py
@@ -0,0 +1,30 @@
+def soma(v1, v2):
+    total = v1 + v2
+    return total
+
+
+print(soma(10, 20))
+
+
+def subtracao(v1, v2):
+    total = v1 - v2
+    return total
+
+
+print(subtracao(10, 20))
+
+
+def multiplicacao(v1, v2):
+    total = v1 * v2
+    return total
+
+
+print(multiplicacao(10, 20))
+
+
+def divisao(v1, v2):
+    total = v1 / v2
+    return total
+
+
+print(divisao(10, 20))
